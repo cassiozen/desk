cc05f3062626232cc64aa6400832a881dd09793b
Cássio Zen <cassiozen@gmail.com>
2014-12-16 15:07:53 -0200

    wildcard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  match '', to: 'home#show', via: [:get, :post], constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
   root 'home#index'
-
-  #match '', to: 'home#show', via: [:get, :post], constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
-
 
 end
