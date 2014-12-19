f8a248d244865f99ec38e2dc5d9206e0a96c8588
Cássio Zen <cassiozen@gmail.com>
2014-12-19 15:30:57 -0200

    index and show depending on subdomain

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
+  skip_filter :scope_current_tenant, :only => :index
+
   def index
+    # No subdomain
   end
 
   def show
-
+    # subdomain specific
   end
 end
