f58b6c1dbdf4304f80c9201f4d0ef601e2f187ef
Cássio Zen <cassiozen@gmail.com>
2014-12-19 15:14:27 -0200

    read messages testing

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
 
+  # The unread gem is already tested, but since messages
+  # are in a polymorphic and indirect association with issues
+  # I felt like testing for my specific scenario
+
   test "user should have unread messages" do
     Tenant.current_id = tenants(:codify).id
     assert_equal 5, issues(:one).messages.unread_by(users(:john)).count
