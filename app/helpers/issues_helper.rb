9e7115de319468c7942b9d557cfa66e005c9caf0
Cássio Zen <cassio@commit.to>
2014-12-30 14:43:23 -0200

    Layout adjustments

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,6 +1,14 @@
 module IssuesHelper
-  def issue_fa_icon(state)
-    state_icons = {"overdue" => "<i class='text-alizarin state fa fa-exclamation-triangle'></i>".html_safe, "pending" => "<i class='text-inverse state fa fa-pause'></i>".html_safe, "open" => "<i class='text-blue state fa fa-clock-o'></i>".html_safe, "due_today" => "<i class='text-midnightblue state fa fa-calendar-o'></i>".html_safe, "closed" => "<i class='text-green state fa fa-check-circle-o'></i>".html_safe}
+  def issue_fa_icon(state, date)
+    color = ""
+    if date.today?
+      color = 'midnightgreen'
+    elsif date > DateTime.now
+      color = 'green'
+    elsif date < DateTime.now
+      color = 'alizarin'
+    end
+    state_icons = {"pending" => "<i class='text-warning state fa fa-pause'></i>".html_safe, "open" => "<i class='text-#{color} state fa fa-exclamation-triangle'></i>".html_safe, "closed" => "<i class='text-inverse state fa fa-check-circle-o'></i>".html_safe}
     state_icons[state]
   end
 
